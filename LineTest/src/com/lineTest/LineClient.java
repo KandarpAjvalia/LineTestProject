dab3dd363221bcd6423bfbd087fd0d3464401ec5
Kandarp Ajvalia <kandarpa9@gmail.com>
2017-11-14 00:07:12 -0500

    Final commit

--- LineTest/src/com/lineTest/LineClient.java
+++ LineTest/src/com/lineTest/LineClient.java
@@ -3,40 +3,53 @@ package com.lineTest;
 import javax.swing.JOptionPane;
 import java.util.*;
 
+/**
+ * A client class for finding if the given two lines by the user are same, parallel, or intersecting
+ */
 public class LineClient
 {
     
-    public static void main(String[] args)
+    private static Scanner in;
+
+	public static void main(String[] args)
     {
-    	Scanner in = new Scanner(System.in);
+    	in = new Scanner(System.in);
     	
-        LineHost line1choice1;
-        LineHost line1choice2;
-        LineHost line1choice3;
-        LineHost line1choice4;
-        LineHost line2choice1;
-        LineHost line2choice2;
-        LineHost line2choice3;
-        LineHost line2choice4;
+    	//Creating null object reference for later use
+        LineHost line1choice1 = null;
+        LineHost line1choice2 = null;
+        LineHost line1choice3 = null;
+        LineHost line1choice4 = null;
+        LineHost line2choice1 = null;
+        LineHost line2choice2 = null;
+        LineHost line2choice3 = null;
+        LineHost line2choice4 = null;
         
-        
+        int choice = 1;
+        int choice2 = 1;
         
         String[] options = new String[] {"Slope and y-intercept", "Two Points", "Slope and Point", "Vertical Line"};
         
+        //For drop down selection menu
         String response  = (String) JOptionPane.showInputDialog(null, "Option Dialog", "Select your option", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
         
         if(response.equals("Slope and y-intercept"))
         {
+        	choice = 1;
             System.out.println("Enter the slope of the line: ");
             double slope = in.nextDouble();
             System.out.println("Enter the y-intercept: ");
             double yIntercept = in.nextDouble();
+            
+            //Constructing a line with given slope and y-intercept
             line1choice1 = new LineHost(slope, yIntercept);
+            
             System.out.println("Your line 1 is y = " + line1choice1.getSlope() + "x + " + line1choice1.getYIntercept());
         }
        
         if(response.equals("Two Points"))
         {
+        	choice = 2;
             System.out.println("Enter the two points\nEnter x1: ");
             double x1 = in.nextDouble();
             System.out.println("Enter y1: ");
@@ -45,7 +58,10 @@ public class LineClient
             double x2 = in.nextDouble();
             System.out.println("Enter y2: ");
             double y2 = in.nextDouble();
+            
+            //Constructing a line with given points
             line1choice2 = new LineHost(x1, y1, x2, y2);
+            
             line1choice2.computeSlope();
             line1choice2.findYInterceot();
             System.out.println("Your line 1 is y = " + line1choice2.getSlope() + "x + " + line1choice2.getYIntercept());
@@ -53,21 +69,29 @@ public class LineClient
         
         if(response.equals("Slope and Point"))
         {
+        	choice = 3;
             System.out.println("Enter the slope of the line: ");
             double slope1 = in.nextDouble();
             System.out.println("Enter the point\nEnter x1: ");
             double secondX1 = in.nextDouble();
             System.out.println("Enter y1: ");
             double secondY1 = in.nextDouble();
+            
+            //Constructing a line with given slope and a point
             line1choice3 = new LineHost(slope1, secondX1, secondY1);
+            line1choice3.findYInterceot();
             System.out.println("Your line 1 is y = " + line1choice3.getSlope() + "x + " + line1choice3.getYIntercept());
         }
         
         if(response.equals("Vertical Line"))
         {
+        	choice = 4;
             System.out.println("Enter the equation of vertical line, x = ");
             double verticalLinePoint = in.nextDouble();
+            
+            //Constructing a vertical line
             line1choice4 = new LineHost(verticalLinePoint);
+            
             System.out.println("The equation of your second line is x = " + line1choice4.verticalLine());
         }
         
@@ -75,16 +99,21 @@ public class LineClient
         
         if(response2.equals("Slope and y-intercept"))
         {
+        	choice2 = 1;
         	System.out.println("Enter the slope of the line: ");
             double slope = in.nextDouble();
             System.out.println("Enter the y-intercept: ");
             double yIntercept = in.nextDouble();
+            
+            //Constructing a second line with given slope and a y-intercept
             line2choice1 = new LineHost(slope, yIntercept);
+            
             System.out.println("Your line 1 is y = " + line2choice1.getSlope() + "x + " + line2choice1.getYIntercept());
         }
        
         if(response2.equals("Two Points"))
         {
+        	choice2 = 2;
         	System.out.println("Enter the two points\nEnter x1: ");
             double x1 = in.nextDouble();
             System.out.println("Enter y1: ");
@@ -93,7 +122,10 @@ public class LineClient
             double x2 = in.nextDouble();
             System.out.println("Enter y2: ");
             double y2 = in.nextDouble();
+            
+            //Constructing a second line with two given points
             line2choice2 = new LineHost(x1, y1, x2, y2);
+            
             line2choice2.computeSlope();
             line2choice2.findYInterceot();
             System.out.println("Your line 1 is y = " + line2choice2.getSlope() + "x + " + line2choice2.getYIntercept());
@@ -102,27 +134,246 @@ public class LineClient
         
         if(response2.equals("Slope and Point"))
         {
+        	choice2 = 3;
             System.out.println("Enter the slope of the line: ");
             double slope1 = in.nextDouble();
             System.out.println("Enter the point\nEnter x1: ");
             double secondX1 = in.nextDouble();
             System.out.println("Enter y1: ");
             double secondY1 = in.nextDouble();
+            
+            //Constructing a second line with given slope and a point
             line2choice3 = new LineHost(slope1, secondX1, secondY1);
+            
+            line2choice3.findYInterceot();
             System.out.println("Your line 1 is y = " + line2choice3.getSlope() + "x + " + line2choice3.getYIntercept());
         }
         
         if(response2.equals("Vertical Line"))
         {
+        	choice2 = 4;
             System.out.println("Enter the equation of vertical line, x = ");
             double verticalLinePoint = in.nextDouble();
+            
+            //Constructing a second vertical line
             line2choice4 = new LineHost(verticalLinePoint);
             System.out.println("The equation of your second line is x = " + line2choice4.verticalLine());
         }
         
+        //Checking different choices by the user
+        
+        if(choice == 1 & choice2 == 1)
+        {
+        	if(line1choice1.getSlope() == line2choice1.getSlope())
+        	{
+        		if(line1choice1.getYIntercept() == line2choice1.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 1 & choice2 == 2)
+        {
+        	if(line1choice1.getSlope() == line2choice2.getSlope())
+        	{
+        		if(line1choice1.getYIntercept() == line2choice2.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 1 & choice2 == 3)
+        {
+        	if(line1choice1.getSlope() == line2choice3.getSlope())
+        	{
+        		if(line1choice1.getYIntercept() == line2choice3.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else 
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 2 & choice2 == 1)
+        {
+        	if(line1choice2.getSlope() == line2choice1.getSlope())
+        	{
+        		if(line1choice2.getYIntercept() == line2choice1.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 2 & choice2 == 2)
+        {
+        	if(line1choice2.getSlope() == line2choice2.getSlope())
+        	{
+        		if(line1choice2.getYIntercept() == line2choice2.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 2 & choice2 == 3)
+        {
+        	if(line1choice2.getSlope() == line2choice3.getSlope())
+        	{
+        		if(line1choice2.getYIntercept() == line2choice3.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 3 & choice2 == 1)
+        {
+        	if(line1choice3.getSlope() == line2choice1.getSlope())
+        	{
+        		if(line1choice3.getYIntercept() == line2choice1.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 3 & choice2 == 2)
+        {
+        	if(line1choice3.getSlope() == line2choice2.getSlope())
+        	{
+        		if(line1choice3.getYIntercept() == line2choice2.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 3 & choice2 == 3)
+        {
+        	if(line1choice3.getSlope() == line2choice3.getSlope())
+        	{
+        		if(line1choice3.getYIntercept() == line2choice3.getYIntercept())
+        		{
+        			same();
+        		}
+        		else
+        		{
+        			parallel();
+        		}
+        	}
+        	else
+        	{
+        		intersecting();
+        	}
+        }
+        
+        if(choice == 4 & choice2 == 4)
+        {
+        	if(line1choice4.verticalLine() == line2choice4.verticalLine())
+        	{
+        		same();
+        	}
+        	else
+        	{
+        		parallel();
+        	}
+        }
+        
+        if((choice == 4 | choice2 == 4) & (choice == 1 | choice==2 | choice == 3 | choice2 == 1 | choice2 == 2 | choice2 == 3))
+        {
+        	//Shows error dialog box if the user chooses vertical line with any different combination than itself
+        	JOptionPane.showMessageDialog(null, "The selected combinaton of lines does not work, terminate the program and re-rum",null, JOptionPane.ERROR_MESSAGE);
+        }
     }
-    
-    
-    
- 
+   
+	/**
+	 * Method to print a String when the given user lines are parallel
+	 */
+	public static void parallel()
+	{
+		System.out.println("The lines are parallel");
+	}
+	
+	/**
+	 * Method to print a String when the given user lines are intersecting
+	 */
+	public static void intersecting()
+	{
+		System.out.println("The lines are intersecting");
+	}
+	
+	/**
+	 * Method to print a String when the given user lines are the same 
+	 */
+	public static void same()
+	{
+		System.out.println("The lines are same");
+	}
+	
 }
--- LineTest/src/com/lineTest/LineHost.java
+++ LineTest/src/com/lineTest/LineHost.java
@@ -1,5 +1,8 @@
 package com.lineTest;
 
+/**
+ * A Host class that supports the constructed lines in the Client class
+ */
 public class LineHost{
 	
 	double hSlope = 0;
@@ -10,47 +13,92 @@ public class LineHost{
 	double hYIntercept = 0;
 	double hVerticalLinePoint = 0;
 	
+	/**
+	 * Constructs a line of a given slope and a y-intercept
+	 * @param slope 
+	 * @param yIntercept
+	 */
 	public LineHost(double slope, double yIntercept)
 	{
 		hSlope = slope;
 		hYIntercept = yIntercept;
 	}
 	
+	/**
+	 * Constructs a line joining given two points
+	 * @param x1 x-coordinate of the first point
+	 * @param y1 y-coordinate of the first point
+	 * @param x2 x-coordinate of the second point
+	 * @param y2 y-coordinate of the second point
+	 */
 	public LineHost(double x1, double y1, double x2, double y2) 
 	{
-		
+		hX1 = x1;
+		hY1 = y1;
+		hX2 = x2;
+		hY2 = y2;
 	}
 	
+	/**
+	 * Constructs a line of a given slope and a point
+	 * @param slope
+	 * @param x x-coordinate of the point
+	 * @param y y-coordinate of the point
+	 */
 	public LineHost(double slope, double x, double y)
 	{
-		
+		hSlope = slope;
+		hX1 = x;
+		hY1 = y;
 	}
 	
+	/**
+	 * Constructs a vertical line with a given x-coordinate
+	 * @param verticalLinePoint
+	 */
 	public LineHost(double verticalLinePoint)
 	{
 		hVerticalLinePoint = verticalLinePoint;
 	}
 	
+	/**
+	 * Method to find the slope from given two points
+	 */
 	public void computeSlope()
 	{
 		hSlope = (hY2 - hY1)/(hX2 - hX1);
 	}
 	
+	/**
+	 * Gets the slope of the line
+	 * @return slope of the line
+	 */
 	public double getSlope() 
 	{
 		return hSlope;
 	}
 	
+	/**
+	 * Method to find the y-intercept of a given line
+	 */
 	public void findYInterceot()
 	{
 		hYIntercept = hY1 - (hSlope * hX1);
 	}
 	
+	/**
+	 * Gets the y-intercept of the line
+	 * @return y-intercept of the line
+	 */
 	public double getYIntercept()
 	{
 		return hYIntercept;
 	}
 	
+	/**
+	 * Gets the equation of vertical line in the form of x = expression 
+	 * @return x value of the vertical line
+	 */
 	public double verticalLine()
 	{
 		return hVerticalLinePoint;
